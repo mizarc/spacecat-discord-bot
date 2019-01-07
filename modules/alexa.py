128d1897a11a8556b5dad9670aebd09b751366b1
Mizarc <mizarc@protonmail.com>
2019-01-07 17:40:54 +1000

    Removed module webhook integration

--- modules/alexa.py
+++ modules/alexa.py
@@ -5,14 +5,10 @@ from discord.ext import commands
 class Alexa:
     def __init__(self, bot):
         self.bot = bot
-        self.webhook = discord.Webhook.from_url(
-            "https://discordapp.com/api/webhooks/503520485901074452/QxrkngBFG4"
-            "vX5tz5Jmn2JRuq3EOG4Rf0yfelIrNWOxAjiydID7co1Ua_mMsh2CTIADnw",
-            adapter=discord.RequestsWebhookAdapter())
 
     async def on_message(self, message):
         if "alexa play despacito" in message.content:
-            self.webhook.send("no")
+            ctx.send("no")
 
 
 def setup(bot):
--- modules/politecat.py
+++ modules/politecat.py
@@ -9,17 +9,13 @@ import deps.perms as perms
 class PoliteCat:
     def __init__(self, bot):
         self.bot = bot
-        self.webhook = discord.Webhook.from_url(
-            "https://discordapp.com/api/webhooks/503811017990471680/-XL-S8muBK"
-            "1Jrz8ND_8ZoAz47hSx84uPbxMmk0IGKG5iNR7S1-W0V-mqfV8YtEEenF_5",
-            adapter=discord.RequestsWebhookAdapter())
 
     @commands.group()
     @perms.admin()
     async def reactcfg(self, ctx):
         "Configure available reaction images"
         if ctx.invoked_subcommand is None:
-            self.webhook.send("I am w o k e")
+            ctx.send("I am w o k e")
 
     @reactcfg.command()
     async def add(self, ctx, name):
@@ -44,9 +40,9 @@ class PoliteCat:
                         await message.attachments[0].save(
                             name + extension)
                     else:
-                        self.webhook.send("Image must be a png or a gif")
+                        ctx.send("Image must be a png or a gif")
                     os.chdir("../")
-                    self.webhook.send("Added *" + name + "* to reactions.")
+                    ctx.send("Added *" + name + "* to reactions.")
                     return
 
     @reactcfg.command()
@@ -67,10 +63,10 @@ class PoliteCat:
             except FileNotFoundError:
                 os.remove(name + ".gif")
         else:
-            self.webhook.send("Reaction image does not exist")
+            ctx.send("Reaction image does not exist")
 
         os.chdir("../")
-        self.webhook.send("Removed *" + name + "* from reactions.")
+        ctx.send("Removed *" + name + "* from reactions.")
         return
 
     @commands.command()
@@ -90,9 +86,9 @@ class PoliteCat:
     async def react(self, ctx, image):
         "Use an image/gif as a reaction"
         try:
-            self.webhook.send(file=discord.File("assets/" + image + ".png"))
+            ctx.send(file=discord.File("assets/" + image + ".png"))
         except FileNotFoundError:
-            self.webhook.send(file=discord.File("assets/" + image + ".gif"))
+            ctx.send(file=discord.File("assets/" + image + ".gif"))
 
 
 def setup(bot):
