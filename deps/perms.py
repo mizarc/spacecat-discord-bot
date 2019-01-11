a5357701ec3127b6d68175b8c8af5eec5aabbd20
Mizarc <mizarc@protonmail.com>
2019-01-11 22:04:53 +1000

    Attempt to get perms to read from config

--- deps/perms.py
+++ deps/perms.py
@@ -1,18 +1,43 @@
 from discord.ext import commands
 from discord.utils import get
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+roles = ['admin', 'mod']
+
+
+def init():
+    for x in roles:
+        try:
+            # Read Config File for API Key
+            perm = config['Perms'][x]
+            return perm
+        except KeyError:
+            config['Perms'] = {}
+            config['Perms'][x] = ''
+    with open('config.ini', 'w') as file:
+        config.write(file)
 
 
 def admin():
     def predicate(ctx):
-        admin_role = "Meme Council"
-        roles = ctx.author.roles
-        return get(roles, name=admin_role)
+        role = "admin"
+        admin_roles = config['Perms'][role].split(',')
+        for x in admin_roles:
+            roles = ctx.author.roles.name
+            if get(roles, name=admin_roles):
+                return True
     return commands.check(predicate)
 
 
 def mod():
     def predicate(ctx):
-        mod_role = "Salt Squad"
-        roles = ctx.author.roles.name
-        return get(roles, name=mod_role)
+        role = "mod"
+        mod_roles = config['Perms'][role].split(',')
+        for x in mod_roles:
+            roles = ctx.author.roles.name
+            if get(roles, name=mod_roles):
+                return True
     return commands.check(predicate)
