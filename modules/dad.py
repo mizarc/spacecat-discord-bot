0f4cfc4b91b082b85d42ed9bcdaf18a6dfca4020
Mizarc <mizarc@protonmail.com>
2019-07-03 23:19:04 +1000

    Fixed the 'dad' module to work as intended

--- modules/dad.py
+++ modules/dad.py
@@ -9,33 +9,37 @@ class Dad(commands.Cog):
         self.channel = 97297058267951104
         self.toggle = True
 
+    @commands.Cog.listener()
     async def on_message(self, message):
-        if message.channel.id == self.channel:
-            if self.toggle:
-                words = message.content.lower().split()
+        if self.toggle:
+            words = message.content.lower().split()
 
-                triggers = ["im", "i'm"]
+            triggers = ["im", "i'm"]
+            print(words)
 
-                for x in triggers:
-                    if x in words[:1]:
-                        qualitycontent = "Hi " + " ".join(words[1:])
-                        + ", I'm Dad!"
+            for x in triggers:
+                if x in words[:1]:
+                    print("yeah")
+                    qualitycontent = "Hi " + " ".join(words[1:]) + ", I'm Dad!"
 
-                        if "dad" in words[:2]:
-                            qualitycontent = "No you're not, I'm dad."
-                    else:
-                        return
+                    if "dad" in words[:2]:
+                        qualitycontent = "No you're not, I'm dad."
+                    break
+                else:
+                    print("nah")
+                    return
 
-                ctx.send(qualitycontent)
+            print(qualitycontent)
+            await message.channel.send(qualitycontent)
 
     @commands.command()
     async def toggledad(self, ctx):
         if self.toggle:
             self.toggle = False
-            ctx.send("Dad has been disabled")
+            await ctx.send("Dad has been disabled")
         elif not self.toggle:
             self.toggle = True
-            ctx.send("Dad has been enabled")
+            await ctx.send("Dad has been enabled")
 
 
 def setup(bot):
