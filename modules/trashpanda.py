7b38c2a54032b38bf24a4a331fece62d49c34c87
Mizarc <mizarc@protonmail.com>
2019-07-07 18:47:05 +1000

    Created 'TrashPanda' module for test features

---
+++ modules/trashpanda.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+
+class TrashPanda(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def echo(self, ctx, channel, *, message):
+        ch = self.bot.get_channel(int(channel))
+        await ch.send(message)
+
+    @commands.command()
+    async def test(self, ctx):
+        await ctx.send(ctx.guild.default_role)
+
+
+def setup(bot):
+    bot.add_cog(TrashPanda(bot))
