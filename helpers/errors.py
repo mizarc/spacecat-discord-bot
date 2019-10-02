b1b679d5f36217c7db38773c175acc07e720fa0c
Mizarc <mizarc@protonmail.com>
2019-10-02 23:54:13 +1000

    Rework error handler to be a cog
    Error handler was originally a helper, which didn't make use of a class
    deriving from cog. This meant that the functions weren't working at all.
    Set to ignore all invalid commands and to tell user when they don't have
    permission to run a command.

--- helpers/errors.py
+++
@@ -1,7 +0,0 @@
-from discord.ext import commands
-
-
-async def on_command_error(ctx, error):
-    printerror = ("`" + str(error) + "`")
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send(printerror)
---
+++ modules/errorhandler.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+class ErrorHandler(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+
+        if isinstance(error, commands.CommandNotFound):
+            return
+
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send("You don't have permission to use that command")
+            return
+
+def setup(bot):
+    bot.add_cog(ErrorHandler(bot))
