a6a0f1e68499a9c5e0517159a927116f39e6c7db
Mizarc <mizarc@protonmail.com>
2019-10-03 16:19:37 +1000

    Add handler to print CommandInvokeError to console
    CommandInvokeErrors are usually raised when an error happens during a
    command execution due to missing dependences or bad code. This should
    assist in discovering errors with the new error handler.

--- modules/errorhandler.py
+++ modules/errorhandler.py
@@ -7,12 +7,15 @@ class ErrorHandler(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-
         if isinstance(error, commands.CommandNotFound):
             return
 
         if isinstance(error, commands.CheckFailure):
             await ctx.send("You don't have permission to use that command")
+            return
+
+        if isinstance(error, commands.CommandInvokeError):
+            print(error)
             return
 
 def setup(bot):
