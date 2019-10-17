057f8fbbdaa63e563e2f8d003614045f427e0767
Mizarc <mizarc@protonmail.com>
2019-10-18 00:24:54 +1000

    Overhaul main function file to be object oriented
    A major change to how the main executed file operates without actually
    breaking any existing configurations or usages. Two objects have been
    created to seperate the bot startup and the commands and listeners whie the
    bot is running. SpaceCat is now treated as its own cog rather than being
    in no category.
    
    Various other changes such as moving the functions related to module
    fetching to its own helper file. Module enabling and disabling has
    become more robust with proper startup messages. F-strings used to
    replace existing formatted strings whenever possible.

---
+++ helpers/module_handler.py
@@ -0,0 +1,33 @@
+import glob
+import os
+
+import discord
+import toml
+
+def get():
+    # Get all modules that are present in the folder
+    modulelist = []
+    for module in glob.glob('modules/*.py'):
+        if module == "modules/__init__.py":
+            continue
+        modulelist.append(module[8:-3])
+    return modulelist
+
+def get_enabled():
+    # Fetch all modules and disable modules
+    modules = get()
+    disabled_modules = get_disabled()
+    enabled_modules = []
+
+    # Compare with disabled modules list to determine which ones are enabled
+    for module in modules:
+        if module not in disabled_modules:
+            enabled_modules.append(module)
+    return enabled_modules
+
+def get_disabled():
+    # Fetch disabled modules from config file
+    config = toml.load('config.toml')
+    disabled_modules = config['base']['disabled_modules']
+    return disabled_modules
+
