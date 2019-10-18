060cfc3057675598c117db003d7e05a97b7c7596
Mizarc <mizarc@protonmail.com>
2019-10-19 02:56:35 +1000

    Rework and fix the setup process to actually work
    Setup has been reorganised to reduce function length and be easy to
    follow within the code. Database has been moved to outside the config
    check, while module loading has been moved to before the config creation
    to load modules properly on first run. Multi-line prints reformatted to
    fit PEP8 guidelines.

--- spacecat.py
+++ spacecat.py
@@ -43,31 +43,37 @@ class Startup():
 
     def create_config(self):
         # Just some info
-        print("Hey there,")
-        print("It appears that you don't have a configuration file.")
-        print("Don't worry, I'll help you set one up in only 3 steps.")
+        print(
+            "Hey there,\n"
+            "It appears that you don't have a configuration file.\n"
+            "Don't worry, I'll help you set one up in only 3 steps.\n")
 
         input("Press Enter to continue...")
         print('--------------------\n')
         time.sleep(1)
 
         # Generate Config
-        print("[Step 1]")
-        print("I'll need to get an API Key from your bot.")
-        print("https://discordapp.com/developers/applications/\n")
-        print("Open that link and follow these instructions:")
-        print("""    1. Create a new application and set a name.
-        2. Open the 'Bot' tab on the left.
-        3. Select 'Create a Bot' and confirm.
-        4. Click on 'Copy' under Token.
-        (Don't ever reveal this token to anyone you don't trust)\n""")
+        print(
+            "[Step 1]\n"
+            "I'll need to get an API Key from your bot.\n"
+            "https://discordapp.com/developers/applications/\n\n"
+            "Open that link and follow these instructions:\n"
+            "1. Create a new application and set a name.\n"
+            "2. Open the 'Bot' tab on the left.\n"
+            "3. Select 'Create a Bot' and confirm.\n"
+            "4. Click on 'Copy' under Token.\n"
+            "(Don't ever reveal this token to anyone you don't trust)\n")
         
         keyinput = input("Paste your token right here: ")
         print('--------------------\n')
 
+        config = {}
         config['base'] = {}
         config['base']['apikey'] = keyinput
-        run(keyinput)
+        with open("config.toml", "w") as config_file:
+            toml.dump(config, config_file)
+
+        self.run(keyinput)
 
     def load_modules(self):
         # Fetch modules and remove disabled modules from enable list
@@ -78,7 +84,7 @@ class Startup():
             for module in modules:
                 if module in self.disabled_modules:
                     modules.remove(module)
-        except KeyError:
+        except (KeyError, FileNotFoundError):
             pass
 
         # Enable modules from list
@@ -100,8 +106,9 @@ class Startup():
             try:
                 bot.run(apikey)
             except discord.LoginFailure:
-                print("""Looks like that API key didn't work.
-            Run the program again and use the correct key.""")
+                print(
+                    "Looks like that API key didn't work.\n"
+                    "Run the program again and use the correct key.")
         # Run with key in the config
         else:
             config = toml.load('config.toml')
@@ -110,10 +117,12 @@ class Startup():
                 print("Active API Key: " + apikey + "\n")
                 bot.run(apikey)
             except discord.LoginFailure:
-                print("""   [Error]
-            The API key doesn't work.
-            Set a new key by running the bot again with the --apikey argument.
-            Eg. ./spacecat --apikey <insert_key>""")
+                print(
+                    "[Error]\n"
+                    "The API key doesn't work.\n"
+                    "Set a new key by running the bot again"
+                    "with the --apikey argument.\n"
+                    "Eg. ./spacecat --apikey <insert_key>")
 
 
 class SpaceCat(commands.Cog):
@@ -122,112 +131,56 @@ class SpaceCat(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        if os.path.exists('config.toml'):
-            config = toml.load('config.toml')
-            print(bot.user.name + " has successfully launched")
-            print(f"Bot ID: {bot.user.id}")
-            if module_handler.get_enabled():
-                print(
-                    "Enabled Module(s): "
-                    f"{', '.join(module_handler.get_enabled())}")
-            if module_handler.get_disabled():
-                print(
-                    "Disabled Module(s): "
-                    f"{', '.join(module_handler.get_disabled())}")
-            print("--------------------")
+        config = toml.load('config.toml')
+        
+        if 'adminuser' not in config['base']:
+            await self._create_config_cont()
 
-            if not os.path.exists("spacecat.db"):
-                perms.setup()
+        if not os.path.exists("spacecat.db"):
+            perms.setup()
 
+        print(bot.user.name + " has successfully launched")
+        print(f"Bot ID: {bot.user.id}")
+        if module_handler.get_enabled():
+            print(
+                "Enabled Module(s): "
+                f"{', '.join(module_handler.get_enabled())}")
+        if module_handler.get_disabled():
+            print(
+                "Disabled Module(s): "
+                f"{', '.join(module_handler.get_disabled())}")
+        print("--------------------")
+
+        try:
             statusname = config['base']['status']
             status = appearance.status_class(statusname)
+            await bot.change_presence(status=status)
+        except KeyError:
+            pass
 
-            try:
-                acttypename = config['base']['activity_type']
-                activitytype = appearance.activity_type_class(acttypename)
+        try:
+            acttypename = config['base']['activity_type']
+            activitytype = appearance.activity_type_class(acttypename)
 
-                activity = discord.Activity(
-                    type=activitytype,
-                    name=config['base']['activity_name'],
-                    url="https://www.twitch.tv/monstercat")
-                await bot.change_presence(
-                    status=status,
-                    activity=activity)
-            except (KeyError, TypeError):
-                await bot.change_presence(status=status)
-        else:
-            while True:
-                # Set a bot admin
-                botname = bot.user.name
-                print("[Step 2]")
-                print(f"Alright, {botname} is now operational.")
-                print("Now I'll need to get your discord user ID.")
-                print("This will give you admin access to the bot.")
-                print("You can set more users later.\n")
-
-                print("Here's what you need to do:")
-                print("""    1. Open Discord.
-        2. Open your user settings.
-        3. Open the appearance tab.
-        4. Enable 'Developer Mode' under Advanced.
-        5. Exit user settings
-        6. Right click on your user and click "Copy ID"\n""")
-
-                idinput = input("Paste your ID right here: ")
-                print('--------------------\n')
-                
-                try:
-                    user = bot.get_user(int(idinput))
-                    await user.send("Hello there!")
-                except (ValueError, AttributeError):
-                    print("It looks like that is not a valid ID.")
-                    print("Lets try this again.")
-                    input("Press Enter to continue...")
-                    print('--------------------\n')
-                    time.sleep(1)
-                    continue
-
-                time.sleep(1)
-                
-                print("You should've recieved a pm from me through Discord.")
-                confirminput = input(
-                    "Type 'yes' if you have, or 'no' to set a new ID: ")
-                print('--------------------\n')
-                if confirminput == "yes":
-                    config['Base']['adminuser'] = idinput
-                    break
-                continue
-
-            config['base']['status'] = 'online'
-            config['base']['activity_type'] = None
-            config['base']['activity_name'] = None
-            toml.dump(config, 'config.toml')
-            time.sleep(1)
-
-            # Join a server
-            print("[Step 3]")
+            activity = discord.Activity(
+                type=activitytype,
+                name=config['base']['activity_name'],
+                url="https://www.twitch.tv/monstercat")
+            await bot.change_presence(activity=activity)
+        except (KeyError, TypeError):
+            pass
             
-            print("Finally, I need to join your Discord server.")
-            print("This is the easiest step.")
-            print("Click the link below or copy it into your web browser.")
-
-            botid = bot.user.id
-            print(
-                f"https://discordapp.com/oauth2/authorize? \
-                client_id={botid}&scope=bot&permissions=8")
-            print('--------------------\n')
-
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
         if os.path.exists('config.ini'):
             perms.new(guild)
         else:
-            print("Congrats! I have now had my core functions set up")
-            print("You may now continue to configure me through discord.")
-            print("Type !help for more info")
-            print('--------------------\n')
-
+            print(
+                "Congrats! I have now had my core functions set up.\n"
+                "You may now continue to configure me through discord.\n"
+                "Type !help for more info\n"
+                "--------------------\n")
 
     # Commands
     @commands.command()
@@ -239,7 +192,6 @@ class SpaceCat(commands.Cog):
             description=f"{bot.user.name} is operational at \
             {int(bot.latency * 1000)}ms")
         await ctx.send(embed=embed)
-
 
     @commands.command()
     @perms.exclusive()
@@ -281,7 +233,6 @@ class SpaceCat(commands.Cog):
                 description=f"Failed to load module {module}")
         await ctx.send(embed=embed)
 
-
     @commands.command()
     @perms.exclusive()
     async def enable(self, ctx, module):
@@ -317,7 +268,6 @@ class SpaceCat(commands.Cog):
                 description=f"Module `{module}` is already enabled")
 
         await ctx.send(embed=embed)
-
 
     @commands.command()
     @perms.exclusive()
@@ -358,7 +308,6 @@ class SpaceCat(commands.Cog):
             description=f"Module `{module}` disabled")
         await ctx.send(embed=embed)
 
-
     @commands.command()
     @perms.exclusive()
     async def exit(self, ctx):
@@ -369,11 +318,79 @@ class SpaceCat(commands.Cog):
             pass
         
         await bot.logout()
+
+    async def _create_config_cont(self):
+        config = toml.load('config.toml')
+        while True:
+            # Set a bot admin
+            botname = bot.user.name
+            print(
+                "[Step 2]\n"
+                f"Alright, {botname} is now operational.\n"
+                "Now I'll need to get your discord user ID.\n"
+                "This will give you admin access to the bot in Discord.\n"
+                "You can set more users later.\n\n"
+
+                "Here's what you need to do:\n"
+                "1. Open Discord.\n"
+                "2. Open your user settings.\n"
+                "3. Open the appearance tab.\n"
+                "4. Enable 'Developer Mode' under Advanced.\n"
+                "5. Exit user settings\n"
+                "6. Right click on your user and click 'Copy ID'\n")
+
+            idinput = input("Paste your ID right here: ")
+            print('--------------------\n')
+            
+            try:
+                user = bot.get_user(int(idinput))
+                await user.send("Hello there!")
+            except (ValueError, AttributeError):
+                print(
+                    "It looks like that is not a valid ID.\n"
+                    "Lets try this again.\n")
+                input("Press Enter to continue...")
+                print('--------------------\n')
+                time.sleep(1)
+                continue
+
+            time.sleep(1)
+            
+            print("You should've recieved a pm from me through Discord.")
+            confirminput = input(
+                "Type 'yes' if you have, or 'no' to set a new ID: ")
+            print('--------------------\n')
+            if confirminput == "yes":
+                config['base']['adminuser'] = idinput
+                break
+            continue
+
+        config['base']['status'] = 'online'
+        config['base']['activity_type'] = None
+        config['base']['activity_name'] = None
+        with open("config.toml", "w") as config_file:
+            toml.dump(config, config_file)
+
+        time.sleep(1)
+
+        # Join a server
+        print("[Step 3]")
+        
+        print("Finally, I need to join your Discord server.")
+        print("This is the easiest step.")
+        print("Click the link below or copy it into your web browser.")
+
+        botid = bot.user.id
+        print(
+            "https://discordapp.com/oauth2/authorize?"
+            f"client_id={botid}&scope=bot&permissions=8")
+        print('--------------------\n')
     
 
 def main():
     startup = Startup()
     startup.logging()
+    startup.load_modules()
 
     # Check if config exists and run config creator if it doesn't
     try:
@@ -386,7 +403,6 @@ def main():
         config['base']['apikey'] = args.apikey
         toml.dump(config, 'config.toml')
     
-    startup.load_modules()
     startup.run()
     
 
