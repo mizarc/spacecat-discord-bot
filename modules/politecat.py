58234201629674e2a2d91631974cf18afded49e9
Mizarc <mizarc@protonmail.com>
2019-10-24 13:21:19 +1000

    Add message if gif conversion failed
    Added message and set gif to be removed in case conversion failed, so as
    to not leave any lingering files in the cache.

--- modules/politecat.py
+++ modules/politecat.py
@@ -24,7 +24,7 @@ class PoliteCat(commands.Cog):
         gif = f'cache/{str(message.id)}.gif'
         webp = f'cache/{str(message.id)}.webp'
 
-        # Convert webp to gif
+        # Convert animated webp to gif
         await message.attachments[0].save(webp)
         image = Image.open(webp)
         try:
@@ -35,8 +35,17 @@ class PoliteCat(commands.Cog):
             file=discord.File(gif))
             os.remove(gif)
             await message.delete()
+
+        # Notify if conversion failed
         except:
-            pass
+            os.remove(gif)
+            embed = discord.Embed(
+                colour=embed_type('warn'),
+                description=f"Failed to convert webp to gif. "
+                "Image may be too large")
+            await message.channel.send(embed=embed) 
+            return
+            
         os.remove(f'cache/{str(message.id)}.webp')
         return
 
