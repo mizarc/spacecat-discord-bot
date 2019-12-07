41a54710598dffa4b6b5d0037820d8f42ac8b0bd
Mizarc <mizarc@protonmail.com>
2019-12-07 15:47:45 +1000

    Create OS specific run files
    Two run files created to run the software from the root directory with
    all the necessary requirements. the sh version is used for unix systems,
    whereas the bat version is for windows systems.

---
+++ run.sh
@@ -0,0 +1,9 @@
+echo "Creating virtual environment..."
+python3 -m venv .venv > /dev/null
+
+echo "Including Requirements..."
+PYTHON=.venv/bin/python3
+$PYTHON -m pip install -e . > /dev/null
+
+cd spacecat
+../$PYTHON spacecat.py
