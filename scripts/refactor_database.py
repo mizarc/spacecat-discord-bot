e1a3ea4e1ee14b05600f49194bce430059811998
Mizarc <mizarc@protonmail.com>
2019-12-27 02:43:47 +1000

    Create script to handle transition process
    Script changes various elements in an existing database. Underscores
    are added to column keys that don't have them for naming consistency.
    Parent and child has been flipped in group_parents table.

---
+++ scripts/refactor_database.py
@@ -0,0 +1,62 @@
+import sqlite3
+
+
+def main():
+    db = sqlite3.connect('../data/spacecat.db')
+    cursor = db.cursor()
+
+    # Command aliases
+    cursor.execute(
+        'ALTER TABLE command_aliases RENAME TO command_aliases_temp')
+    cursor.execute((
+        'CREATE TABLE command_aliases'
+        '(server_id INTEGER, alias TEXT, command TEXT)'))
+    cursor.execute((
+        'INSERT INTO command_aliases(server_id, alias, command)'
+        'SELECT server_id, alias, command FROM command_aliases_temp'))
+    cursor.execute('DROP TABLE command_aliases_temp')
+    print("Command aliases db transfer complete.")
+
+    # Group permissions
+    cursor.execute(
+        'ALTER TABLE group_permissions RENAME TO group_permissions_temp')
+    cursor.execute((
+        'CREATE TABLE group_permissions'
+        '(server_id INTEGER, group_id INTEGER, perm TEXT)'))
+    cursor.execute((
+        'INSERT INTO group_permissions(server_id, group_id, perm)'
+        'SELECT serverid, groupid, perm FROM group_permissions_temp'))
+    cursor.execute('DROP TABLE group_permissions_temp')
+    print("Group permissions db transfer complete.")
+
+    # User permissions
+    cursor.execute(
+        'ALTER TABLE user_permissions RENAME TO user_permissions_temp')
+    cursor.execute((
+        'CREATE TABLE user_permissions'
+        '(server_id INTEGER, user_id INTEGER, perm TEXT)'))
+    cursor.execute((
+        'INSERT INTO user_permissions(server_id, user_id, perm)'
+        'SELECT serverid, userid, perm FROM user_permissions_temp'))
+    cursor.execute('DROP TABLE user_permissions_temp')
+    print("User permissions db transfer complete.")
+
+    # Group parents
+    cursor.execute(
+        'ALTER TABLE group_parents RENAME TO group_parents_temp')
+    cursor.execute((
+        'CREATE TABLE group_parents'
+        '(server_id INTEGER, parent_group INTEGER, child_group INTEGER)'))
+    cursor.execute((
+        'INSERT INTO group_parents(server_id, parent_group, child_group)'
+        'SELECT serverid, parent_group, child_group FROM group_parents_temp'))
+    cursor.execute('DROP TABLE group_parents_temp')
+    print("Group parents db transfer complete.")
+
+    db.commit()
+    db.close()
+    print("Changes Finalised.")
+
+
+if __name__ == "__main__":
+    main()
