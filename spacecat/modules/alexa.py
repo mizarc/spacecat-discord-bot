f981401ddd39bafc7dfc875bf4f2f944aec16343
Mizarc <mizarc@protonmail.com>
2020-01-04 19:22:44 +1000

    Add playlist name to songlist

--- spacecat/modules/alexa.py
+++ spacecat/modules/alexa.py
@@ -755,7 +755,8 @@ class Alexa(commands.Cog):
         formatted_duration = await self._get_duration(total_duration)
         playlist_music_output = '\n'.join(formatted_songs)
         embed.add_field(
-            name=f"{len(songs)} available `{formatted_duration}`",
+            name=f"{len(songs)} songs available in "
+            f"{playlist} `{formatted_duration}`",
             value=playlist_music_output, inline=False)
         await ctx.send(file=image, embed=embed)
 
