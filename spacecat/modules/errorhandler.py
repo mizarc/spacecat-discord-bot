954e75be3c3d52f98045bd7a1939970bad74932a
Mizarc <mizarc@protonmail.com>
2020-02-08 00:59:10 +1000

    Add error messages for bot perms and missing args
    Should've added this earlier. A message to tell user that the command
    they've entered is missing arguments rather than outputting nothing.
    Also an output for if the bot doesn't have the necessary server
    perms to execute the command.

--- spacecat/modules/errorhandler.py
+++ spacecat/modules/errorhandler.py
@@ -14,7 +14,25 @@ class ErrorHandler(commands.Cog):
             return
 
         if isinstance(error, commands.CheckFailure):
-            embed = discord.Embed(colour=settings.embed_type('warn'), description="You don't have permission to use that command")
+            embed = discord.Embed(
+                colour=settings.embed_type('warn'),
+                description="You don't have permission to use that command")
+            await ctx.send(embed=embed)
+            return
+
+        if isinstance(error, commands.BotMissingPermissions):
+            embed = discord.Embed(
+                colour=settings.embed_type('warn'),
+                description="I don't have the necessary server permission"
+                "to execute that. Contact the server administrator.")
+            await ctx.send(embed=embed)
+            return
+
+        if isinstance(error, commands.MissingRequiredArgument):
+            embed = discord.Embed(
+                colour=settings.embed_type('warn'),
+                description="Missing Arguments. "
+                f"Type `{ctx.prefix}help {ctx.command}` for more info")
             await ctx.send(embed=embed)
             return
 
