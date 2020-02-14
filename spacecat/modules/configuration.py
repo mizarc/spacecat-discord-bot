87e4d98bfdfd7d7889cb7a00246402a2d7c0c951
Mizarc <mizarc@protonmail.com>
2020-02-14 13:25:27 +1000

    Add permpreset add command
    Taking influence from the server only 'perm' command group, the
    'permpreset add' subcommand is used to add permissions to an existing
    preset. You can add a wildcard, a module, or just a specific
    command. Checks have been provided to ensure that permissions being
    added exist as an actual command or module.

--- spacecat/modules/configuration.py
+++ spacecat/modules/configuration.py
@@ -166,8 +166,94 @@ class Configuration(commands.Cog):
 
     @permpreset.command(name='add')
     @perms.exclusive()
-    async def permpreset_add(self, ctx):
-        print('nah')
+    async def permpreset_add(self, ctx, preset, perm):
+        config = toml.load(settings.data + 'config.toml')
+        perm_values = perm.split('.')
+        skip = False
+
+        # Add wildcard permission if it doesn't already exist
+        if perm == '*':
+            if perm in config['permissions'][preset]:
+                embed = discord.Embed(
+                    colour=settings.embed_type('warn'),
+                    description=f"Permission preset `{preset}` "
+                    "already has the wildcard permission")
+                await ctx.send(embed=embed)
+                return
+            else:
+                config['permissions'][preset].append('*')
+                embed = discord.Embed(
+                    colour=settings.embed_type('accept'),
+                    description="Wildcard permission added to "
+                    f"permission preset `{preset}`")
+                await ctx.send(embed=embed)
+                skip = True
+
+        # Check if permission starts with a cog
+        if not skip and len(perm_values) > 1:
+            cog = self.bot.get_cog(perm_values[0])
+
+            # Check if non-wildcard permission has been chosen and move onto
+            # the next section for command level perm handling
+            if cog and perm_values[1] != '*':
+                perm_values.pop(0)
+     
+            # Add permission group permission if it doesn't already exist
+            elif cog and perm in config['permissions'][preset]:
+                embed = discord.Embed(
+                    colour=settings.embed_type('warn'),
+                    description=f"`{preset}` already has the"
+                    f"`{cog.qualified_name}` permission group")
+                await ctx.send(embed=embed) 
+                return
+            elif cog and perm not in config['permissions'][preset]:
+                config['permissions'][preset].append(
+                    f"{cog.qualified_name}.*")
+                embed = discord.Embed(
+                    colour=settings.embed_type('accept'),
+                    description=f"Permission group `{cog.qualified_name}`"
+                    f" added to preset `{preset}`")
+                await ctx.send(embed=embed)
+                skip = True
+
+        # Check if permission is a command
+        if not skip:
+            command_parents = perm.split('.')
+            
+            # Exclude subcommand wildcard when checking if command exists
+            if command_parents[-1] == '*':
+                command = self.bot.get_command(perm[:-2].replace('.', ' '))
+            else:
+                command = self.bot.get_command(perm.replace('.', ' '))
+
+            # Alert if no permission goes by that name after checking
+            # both module and command names
+            if not command:
+                embed = discord.Embed(
+                    colour=settings.embed_type('warn'),
+                    description=f"Permission does not exist."
+                    "Please enter a valid permission")
+                await ctx.send(embed=embed) 
+                return
+
+            # Add command permission if it doesn't already exist
+            full_permission = f"{command.cog.qualified_name}.{perm}"
+            if full_permission in config['permissions'][preset]:
+                embed = discord.Embed(
+                    colour=settings.embed_type('warn'),
+                    description=f"`{preset}` already has that permission")
+                await ctx.send(embed=embed) 
+                return
+            else:
+                config['permissions'][preset].append(
+                    f"{command.cog.qualified_name}.{perm}")
+                embed = discord.Embed(
+                    colour=settings.embed_type('accept'),
+                    description=f"Command `{perm}` added to preset `{preset}`")
+                await ctx.send(embed=embed)
+            
+        with open(settings.data + "config.toml", "w") as config_file:
+            toml.dump(config, config_file)
 
     @permpreset.command(name='remove')
     @perms.exclusive()
