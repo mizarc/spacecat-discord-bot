f68c316b1858cfdffbb0f798b593aab692325dbb
Mizarc <mizarc@protonmail.com>
2020-02-28 13:45:22 +1000

    Display new instance list after create/destroy
    The list of instances and available options have been moved to the
    instance module, under the function display(). This allows both the
    create and destroy functions to show the new list of instances after
    they have been used.

--- spacecat/instance.py
+++ spacecat/instance.py
@@ -1,7 +1,23 @@
 import shutil
 import os
 
-from spacecat.helpers import constants
+from spacecat.helpers import config, constants
+
+
+def display():
+    """Prints a list of instances and other instance editing options"""
+    instances = get()
+    print("[Available Instances]")
+    
+    # Add list of instances, plus extra options
+    formatted_instances = []
+    for index, inst in enumerate(instances):
+        formatted_instances.append(f'{index + 1}. {inst}')
+    print('\n'.join(formatted_instances))
+    print("\n[Other Options]")
+    print("n. NEW INSTANCE")
+    print("r. REMOVE INSTANCE")
+    print("e. EXIT\n")
 
 
 def get():
@@ -18,6 +34,10 @@ def create():
     """Creates a new instance folder"""
     name = input("Specify the new instance name: ")
     print('--------------------\n')
+    constants.data_location(name)
+    config.create()
+    constants.data_location('')
+    display()
     return name
     
 
@@ -27,4 +47,5 @@ def destroy(instances):
     print('--------------------\n')
     selected_instance = instances[index - 1]
     shutil.rmtree(constants.DATA_DIR + selected_instance)
+    display()
     return
