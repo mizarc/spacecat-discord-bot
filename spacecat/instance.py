a7119e598858c6958ae6d5b8ce0e5408618ef87a
Mizarc <mizarc@protonmail.com>
2020-04-27 20:31:01 +1000

    Reconfigure instance module to remove prompts
    Rather than utilising the instance method to hold instance specific
    cli prompts, it is now used to directly run instance based actions with
    provided arguments. All prompts have been moved into the cli module,
    serving as their own seperate functions.

--- spacecat/instance.py
+++ spacecat/instance.py
@@ -4,68 +4,34 @@ import os
 from spacecat.helpers import config, constants
 
 
-def display():
-    """Prints a list of instances and other instance editing options"""
-    instances = get()
-    print("[Available Instances]")
-    
-    # Add list of instances, plus extra options
-    formatted_instances = []
-    for index, inst in enumerate(instances):
-        formatted_instances.append(f'{index + 1}. {inst}')
-    print('\n'.join(formatted_instances))
-    print("\n[Other Options]")
-    print("n. NEW INSTANCE")
-    print("r. REMOVE INSTANCE")
-    print("x. EXIT\n")
-
-
 def get():
     """Checks for a config file in each subfolder to detect an instance"""
-    instances = []
     folders = os.listdir(constants.DATA_DIR)
-    for content in folders:
-        if os.path.isfile(f'{constants.DATA_DIR}{content}/config.toml'):
-            instances.append(content)
+    instances = []
+    for folder in folders:
+        if os.path.isfile(f'{constants.DATA_DIR}{folder}/config.toml'):
+            instances.append(folder)
+            
     return instances
 
 
-def create():
+def create(name):
     """Creates a new instance folder"""
-    name = input("Specify the new instance name: ")
-    print('--------------------\n')
-    constants.data_location(name)
-    config.create()
-    constants.data_location('')
-    display()
-    return name
+    try:
+        os.mkdir(f'{constants.DATA_DIR}{name}')
+    except FileExistsError:
+        return False
+
+    return True
     
 
-def destroy(instances):
+def destroy(instances, index):
     """Deletes an instance folder by the index"""
-    while True:
-        index = int(input("Specify the instance number to delete: "))
-        print('--------------------\n')
+    # Check if selected instance is valid
+    try:
+        selected_instance = instances[index - 1]
+    except IndexError:
+        return False
 
-        # Check if selected instance is valid
-        try:
-            selected_instance = instances[index - 1]
-        except IndexError:
-            print("Invalid instance number. Moved back to main menu.\n")
-            display()
-            return
-
-        # Ask to confirm instance deletion
-        confirm = input("Are you sure you want to delete that instance? (y/n): ")
-        print('--------------------\n')
-
-        if confirm == 'y':
-            break
-        elif confirm == 'n':
-            return
-        else:
-            print("Invalid option.")
-
-    shutil.rmtree(constants.DATA_DIR + selected_instance)
-    display()
-    return
+    shutil.rmtree(f'{constants.DATA_DIR}{selected_instance}')
+    return True
