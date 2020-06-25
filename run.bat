60f934343ea7dadc8e7e69b686a290b97931d70d
Mizarc <mizarc@protonmail.com>
2020-06-25 21:16:42 +1000

    Remove run and upgrade scripts
    As the upgrade scripts are not required to upgrade from the previous
    major versions, they have been removed as anyone currently running this
    would've probably upgraded their databases using the scripts by now.
    The start scripts have been removed and instructions for running
    in a virtual environment if necessary will be provided.

--- run.bat
+++
@@ -1,8 +0,0 @@
-echo Creating virtual environment...
-py -3 -m venv .venv
-
-echo Including Requirements...
-SET PYTHON=.venv\Scripts\python.exe
-%PYTHON% -m pip install -e .
-
-%PYTHON% -m spacecat %*
--- run.sh
+++
@@ -1,8 +0,0 @@
-echo "Creating virtual environment..."
-python3 -m venv .venv\
-
-echo "Including Requirements..."
-PYTHON=.venv/bin/python3
-$PYTHON -m pip install -e .
-
-$PYTHON -m spacecat $@
