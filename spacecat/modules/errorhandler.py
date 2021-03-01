8480e59517c2fe6bdbeb60f08b60c73c9bc747e6
Mizarc <mizarc@protonmail.com>
2021-03-02 03:10:24 +1000

    Modify the error handler to handle slash commands
    While the slash command error handler makes use of the same decorator,
    the method name itself and the error instances it goes by are different.
    Most of the previous checks can be ignored as they are handled on the
    discord client itself, though checking for user and bot permission is
    still required.

--- spacecat/modules/errorhandler.py
+++ spacecat/modules/errorhandler.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord_slash import error as dserror
 
 from spacecat.helpers import constants
 
@@ -10,43 +11,20 @@ class ErrorHandler(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.CommandNotFound):
-            return
-
-        if isinstance(error, commands.CheckFailure):
+    async def on_slash_command_error(self, ctx, error):
+        if isinstance(error, dserror.CheckFailure):
             embed = discord.Embed(
                 colour=constants.EmbedStatus.FAIL.value,
                 description="You don't have permission to use that command")
             await ctx.send(embed=embed)
             return
 
-        if isinstance(error, commands.BotMissingPermissions):
+        if isinstance(error, discord.Forbidden):
             embed = discord.Embed(
                 colour=constants.EmbedStatus.FAIL.value,
                 description="I don't have the necessary server permission"
                 "to execute that. Contact the server administrator.")
             await ctx.send(embed=embed)
-            return
-
-        if isinstance(error, commands.MissingRequiredArgument):
-            embed = discord.Embed(
-                colour=constants.EmbedStatus.FAIL.value,
-                description="Missing Arguments. "
-                f"Type `{ctx.prefix}help {ctx.command}` for more info")
-            await ctx.send(embed=embed)
-            return
-
-        if isinstance(error, commands.BadArgument):
-            embed = discord.Embed(
-                colour=constants.EmbedStatus.FAIL.value,
-                description="Invalid arguments provided. "
-                f"Type `{ctx.prefix}help {ctx.command}` for more info")
-            await ctx.send(embed=embed)
-            return
-
-        if isinstance(error, commands.CommandInvokeError):
-            print(error)
             return
 
 
