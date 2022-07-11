3072217858e787ca44c265f8f91d8e9d3b8d3af8
Mizarc <mizarc@protonmail.com>
2022-07-11 15:35:51 +1000

    Port seethreepio to new slash command system
    Additional change is that adding a cog must be awaited and therefore the
    setup function has been changed to async.

--- spacecat/modules/seethreepio.py
+++ spacecat/modules/seethreepio.py
@@ -1,6 +1,6 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
-from discord_slash import cog_ext, SlashContext
 
 from spacecat.helpers import perms
 
@@ -10,45 +10,43 @@ class Seethreepio(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @cog_ext.cog_slash()
+    @app_commands.command()
     @perms.check()
-    async def echo(self, ctx: SlashContext, *, message):
+    async def echo(self, interaction, *, message: str):
         """Repeats a given message"""
-        await ctx.respond()
-        await ctx.send(message)
+        await interaction.response.send_message(message)
 
-    @cog_ext.cog_slash()
+    @app_commands.command()
     @perms.check()
-    async def flip(self, ctx: SlashContext, member: discord.Member=None):
+    async def flip(self, interaction, member: discord.Member = None):
         """Flips a table... Or a person"""
-        if member == None:
-            await ctx.send("(╯°□°）╯︵ ┻━┻")
+        if member is None:
+            await interaction.response.send_message("(╯°□°）╯︵ ┻━┻")
             return
 
         if member.id != self.bot.user.id:
-            await ctx.send("(╯°□°）╯︵ " + member.mention)
+            await interaction.response.send_message("(╯°□°）╯︵ " + member.mention)
         else:
-            await ctx.send("Bitch please. \n'(╯°□°）╯︵ "
-                           + ctx.message.author.mention)
+            await interaction.response.send_message("Bitch please. \n'(╯°□°）╯︵ " + interaction.user.mention)
 
-    @cog_ext.cog_slash()
+    @app_commands.command()
     @perms.check()
-    async def throw(self, ctx, member: discord.Member, *, item=None):
+    async def throw(self, interaction, member: discord.Member, *, item: str = None):
         if item is not None:
-            await ctx.send("(∩⚆ᗝ⚆)⊃ --==(" + item + ")     "
+            await interaction.response.send_message("(∩⚆ᗝ⚆)⊃ --==(" + item + ")     "
                            + member.mention)
         else:
             if member.id != self.bot.user.id:
-                await ctx.send("(∩⚆ᗝ⚆)⊃ --==(O)     " + member.mention)
+                await interaction.response.send_message("(∩⚆ᗝ⚆)⊃ --==(O)     " + member.mention)
             else:
-                await ctx.send("Bitch please. \n'(∩⚆ᗝ⚆)⊃ --==(O)     "
-                               + ctx.message.author.mention)
+                await interaction.response.send_message("Bitch please. \n'(∩⚆ᗝ⚆)⊃ --==(O)     "
+                               + interaction.user.mention)
 
-    @cog_ext.cog_slash()
+    @app_commands.command()
     @perms.check()
-    async def stealuserpic(self, ctx, user: discord.User):
-        await ctx.send(user.avatar_url)
+    async def stealuserpic(self, interaction, user: discord.User):
+        await interaction.response.send_message(user.avatar_url)
 
 
-def setup(bot):
-    bot.add_cog(Seethreepio(bot))
+async def setup(bot):
+    await bot.add_cog(Seethreepio(bot))
