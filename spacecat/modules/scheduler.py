ec987408c1029a2ae38eff76d1c18b8e27988d8d
Mizarc <mizarc@protonmail.com>
2022-08-15 23:41:29 +1000

    Create Scheduler module to run events later
    This module's purpose is to allow users to create events to be run later
    or at set intervals. This can be something as simple as a message
    reminder or user settings manipulation.
    
    This currently implements the base "Scheduler" class to hold commands
    and listeners, as well as a simple "Reminder" class to hold messages to
    be sent later.

---
+++ spacecat/modules/scheduler.py
@@ -0,0 +1,28 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+import uuid
+
+
+class Reminder:
+    def __init__(self, id_, user_id, channel_id, time, message):
+        self.id = id_
+        self.user_id = user_id
+        self.channel_id = channel_id
+        self.time = time
+        self.message = message
+
+    @classmethod
+    def create_new(cls, user, channel, time, message):
+        return cls(uuid.uuid4(), user.id, channel.id, time, message)
+
+
+class Scheduler(commands.Cog):
+    """Schedule events to run at a later date"""
+    def __init__(self, bot):
+        self.bot = bot
+
+
+async def setup(bot):
+    await bot.add_cog(Scheduler(bot))
