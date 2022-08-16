a141ba7405609a0be82adefed82a5047fc2a8ec4
Mizarc <mizarc@protonmail.com>
2022-08-16 19:54:49 +1000

    Add Reminder Repository class
    This will provide the necessary database calls to add, remove, update,
    and read reminder data.

--- spacecat/modules/scheduler.py
+++ spacecat/modules/scheduler.py
@@ -1,9 +1,16 @@
+import asyncio
+import sqlite3
+
 import discord
 from discord import app_commands
-from discord.ext import commands
+from discord.ext import commands, tasks
 
+import datetime
 import time
 import uuid
+
+from spacecat.helpers import constants
+from spacecat.spacecat import SpaceCat
 
 
 class Reminder:
@@ -18,6 +25,77 @@ class Reminder:
     @classmethod
     def create_new(cls, user, channel, timestamp, message):
         return cls(uuid.uuid4(), user.id, channel.id, timestamp, message)
+
+
+class ReminderRepository:
+    def __init__(self, database):
+        self.db = database
+        cursor = self.db.cursor()
+        cursor.execute('PRAGMA foreign_keys = ON')
+        cursor.execute('CREATE TABLE IF NOT EXISTS reminders (id TEXT PRIMARY KEY, user_id INTEGER, guild_id INTEGER, '
+                       'channel_id INTEGER, timestamp INTEGER, message TEXT)')
+        self.db.commit()
+
+    def get_all(self):
+        """Get list of all playlists"""
+        results = self.db.cursor().execute('SELECT * FROM reminders').fetchall()
+        playlists = []
+        for result in results:
+            playlists.append(Reminder(result[0], result[1], result[2], result[3], result[4], result[5]))
+        return playlists
+
+    def get_by_id(self, id_):
+        result = self.db.cursor().execute('SELECT * FROM reminders WHERE id=?', (id_,)).fetchone()
+        return Reminder(result[0], result[1], result[2], result[3], result[4], result[5])
+
+    def get_by_guild(self, guild):
+        # Get list of all playlists in a guild
+        cursor = self.db.cursor()
+        values = (guild.id,)
+        cursor.execute('SELECT * FROM reminders WHERE guild_id=?', values)
+        results = cursor.fetchall()
+
+        playlists = []
+        for result in results:
+            playlists.append(Reminder(result[0], result[1], result[2], result[3], result[4], result[5]))
+        return playlists
+
+    def get_by_guild_and_user(self, guild, name):
+        # Get playlist by guild and playlist name
+        cursor = self.db.cursor()
+        values = (guild.id, name)
+        cursor.execute('SELECT * FROM reminders WHERE guild_id=? AND user_id=?', values)
+        results = cursor.fetchall()
+
+        playlists = []
+        for result in results:
+            playlists.append(Reminder(result[0], result[1], result[2], result[3], result[4], result[5]))
+        return playlists
+
+    def get_first_before_timestamp(self, timestamp):
+        cursor = self.db.cursor()
+        result = cursor.execute('SELECT * FROM reminders WHERE timestamp < ? ORDER BY timestamp',
+                                (timestamp,)).fetchone()
+        return Reminder(result[0], result[1], result[2], result[3], result[4], result[5])
+
+    def add(self, reminder):
+        cursor = self.db.cursor()
+        values = (str(reminder.id), reminder.name, reminder.guild_id, reminder.description)
+        cursor.execute('INSERT INTO reminders VALUES (?, ?, ?, ?, ?, ?)', values)
+        self.db.commit()
+
+    def update(self, reminder):
+        cursor = self.db.cursor()
+        values = (reminder.guild_id, reminder.name, reminder.description, reminder.id)
+        cursor.execute('UPDATE reminders SET user_id=?, guild_id=?, channel_id=?, '
+                       'timestamp=?, message=? WHERE id=?', values)
+        self.db.commit()
+
+    def remove(self, reminder):
+        cursor = self.db.cursor()
+        values = (reminder.id,)
+        cursor.execute('DELETE FROM reminders WHERE id=?', values)
+        self.db.commit()
 
 
 class Scheduler(commands.Cog):
