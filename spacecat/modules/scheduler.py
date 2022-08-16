08c2d59835b470b247cc8526a35e7ed9b9e6f917
Mizarc <mizarc@protonmail.com>
2022-08-16 14:57:36 +1000

    Add 'remindme' command with timestamp conversion
    To test the user facing interaction with the reminder system, a command
    has been created with all the arguments a user could need to specify
    when the reminder should be sent out. Each time based argument is
    optional.
    
    A static method to convert these intervals to a seconds based timestamp
    has been added to be used by this command.

--- spacecat/modules/scheduler.py
+++ spacecat/modules/scheduler.py
@@ -24,6 +24,24 @@ class Scheduler(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @app_commands.command()
+    async def remindme(self, interaction, message: str, seconds: int = None, minutes: int = None, hours: int = None,
+                       days: int = None, weeks: int = None, months: int = None, years: int = None):
+
+        timestamp = await self.to_seconds(seconds, minutes, hours, days, weeks, months, years) + time.time()
+        reminder = Reminder.create_new(interaction.user, interaction.channel, timestamp, message)
+
+    @staticmethod
+    async def to_seconds(seconds, minutes, hours, days, weeks, months, years) -> int:
+        total = seconds
+        total += minutes * 60
+        total += hours * 3600
+        total += days * 86400
+        total += weeks * 604800
+        total += months * 2629800
+        total += years * 31557600
+        return total
+
 
 async def setup(bot):
     await bot.add_cog(Scheduler(bot))
