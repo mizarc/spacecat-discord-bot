a2611556337d3ce65b80af11721ebc60e4fb4ac2
Mizarc <mizarc@protonmail.com>
2022-08-16 14:52:14 +1000

    Rename time to timestamp to avoid conflicts
    Due to the time package now being in use, the existing time parameters
    have been renamed to timestamp.

--- spacecat/modules/scheduler.py
+++ spacecat/modules/scheduler.py
@@ -2,20 +2,21 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
+import time
 import uuid
 
 
 class Reminder:
-    def __init__(self, id_, user_id, channel_id, time, message):
+    def __init__(self, id_, user_id, channel_id, timestamp, message):
         self.id = id_
         self.user_id = user_id
         self.channel_id = channel_id
-        self.time = time
+        self.timestamp = timestamp
         self.message = message
 
     @classmethod
-    def create_new(cls, user, channel, time, message):
-        return cls(uuid.uuid4(), user.id, channel.id, time, message)
+    def create_new(cls, user, channel, timestamp, message):
+        return cls(uuid.uuid4(), user.id, channel.id, timestamp, message)
 
 
 class Scheduler(commands.Cog):
