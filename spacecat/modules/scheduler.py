e6a593542e9d32395f9227036541c52c846e58c3
Mizarc <mizarc@protonmail.com>
2022-08-17 02:42:34 +1000

    Send formatted datetime on reminder confirmation

--- spacecat/modules/scheduler.py
+++ spacecat/modules/scheduler.py
@@ -140,12 +140,18 @@ class Scheduler(commands.Cog):
     async def remindme(self, interaction, message: str, seconds: int = 0, minutes: int = 0, hours: int = 0,
                        days: int = 0, weeks: int = 0, months: int = 0, years: int = 0):
 
-        timestamp = await self.to_seconds(seconds, minutes, hours, days, weeks, months, years) + time.time()
-        reminder = Reminder.create_new(interaction.user, interaction.guild, interaction.channel, timestamp, message)
+        timestamp = await self.to_seconds(seconds, minutes, hours, days, weeks, months, years)
+        dispatch_time = timestamp + time.time()
+        reminder = Reminder.create_new(interaction.user, interaction.guild, interaction.channel, dispatch_time, message)
         self.reminders.add(reminder)
         self.reminder_task.cancel()
         self.reminder_task = self.bot.loop.create_task(self.reminder_loop())
-        await interaction.response.send_message("Woop")
+
+        embed = discord.Embed(
+            colour=constants.EmbedStatus.INFO.value,
+            description=f"Reminder has been set for "
+                        f"{await self.format_datetime(datetime.timedelta(seconds=timestamp))}")
+        await interaction.response.send_message(embed=embed)
 
     @staticmethod
     async def to_seconds(seconds=0, minutes=0, hours=0, days=0, weeks=0, months=0, years=0) -> int:
