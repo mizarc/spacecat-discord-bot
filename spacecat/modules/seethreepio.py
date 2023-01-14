adf0036eef4dcaf244f6ab92c1c896a2b8f5e4d4
Mizarc <mizarc@proton.me>
2023-01-14 17:53:00 +1000

    Update ascii messages of throw command
    The ascii art has a new look to it, including a little character of the
    user it's being thrown at to convey more emotion. The message is also
    going to be updated gradually to show the item being animated towards
    the user.

--- spacecat/modules/seethreepio.py
+++ spacecat/modules/seethreepio.py
@@ -1,8 +1,10 @@
+import asyncio
 import time
 
 import discord
 from discord import app_commands
 from discord.ext import commands
+from discord.ui import View, Button
 
 from spacecat.helpers import perms
 
@@ -12,6 +14,16 @@ class Throwing:
         self.thrower = thrower
         self.target = target
         self.timeout_time = time.time() + 5.0
+
+        # Current State
+        self.caught = False
+
+class CatchButton(Button):
+    def __init__(self, throwing):
+        self.throwing = throwing
+
+    async def callback(self, interaction):
+        self.throwing.caught = True
 
 
 class Seethreepio(commands.Cog):
@@ -42,15 +54,35 @@ class Seethreepio(commands.Cog):
     @app_commands.command()
     @perms.check()
     async def throw(self, interaction: discord.Interaction, member: discord.Member, *, item: str = None):
+        if item is None:
+            item = "O"
+
+        # Have the bot throw the item at the user if the bot is targeted
         if member.id == self.bot.user.id:
-            await interaction.response.send_message("No u. \n'(∩⚆ᗝ⚆)⊃ --==(O)     " + interaction.user.mention)
+            await interaction.response.send_message(f"No u. \n'(∩òᗝó)⊃ --==({item})          "
+                                                    f"∩(óᗝò)∩ {interaction.user.mention}")
             return
 
-        if item is not None:
-            await interaction.response.send_message("(∩⚆ᗝ⚆)⊃ --==(" + item + ")     " + member.mention)
-        else:
-            await interaction.response.send_message("(∩⚆ᗝ⚆)⊃ --==(O)     " + member.mention)
-        self.throwings[member.id] = Throwing(interaction.user, member)
+        # Throw the item, giving the target a prompt to catch it
+        throwing = Throwing(interaction.user, member)
+        await interaction.response.defer()
+        await interaction.edit_original_response(
+            content=f"{interaction.user.mention} (∩òᗝó)⊃ --==({item})          ∩(óᗝò)∩ " + member.mention)
+
+        await asyncio.sleep(3)
+        await interaction.edit_original_response(
+            content=f"{interaction.user.mention} (∩òᗝó)⊃      --==({item})     ∩(óᗝò)∩ {member.mention}")
+
+        await asyncio.sleep(3)
+        if throwing.caught:
+            await interaction.edit_original_response(
+                content=f"{interaction.user.mention} (∩óᗝò)⊃             "
+                        f"({item})⸦(òᗝó⸦) {member.mention} has caught it!")
+            return
+        await interaction.edit_original_response(
+            content=f"{interaction.user.mention} (∩òᗝó)⊃                "
+                    f"--==({item})Д⨱)∩ {member.mention} got dunked!")
+
 
     @app_commands.command()
     @perms.check()
