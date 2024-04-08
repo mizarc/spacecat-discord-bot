b6985c01557bf7afc33d4bbe3d215614acb5e9bf
DESKTOP-939K91A\Mizarc <mizarc@proton.me>
2024-04-09 01:14:32 +1000

    Fix call to playing property for auto disconnect
    This had to be changed to a property call rather than a function call.

--- spacecat/modules/musicbox.py
+++ spacecat/modules/musicbox.py
@@ -758,7 +758,7 @@ class WavelinkMusicPlayer(MusicPlayer[WavelinkSong]):
 
     @tasks.loop(seconds=30)
     async def _disconnect_job(self):
-        if self._is_auto_disconnect() and time() > self._disconnect_time and not self._player.playing():
+        if self._is_auto_disconnect() and time() > self._disconnect_time and not self._player.playing:
             await self.disconnect()
 
     def _refresh_disconnect_timer(self):
