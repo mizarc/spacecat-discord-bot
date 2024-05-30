120aa65adee5b0563dc9b9ee0a2d4fdd00d9ed73
Mizarc <mizarc@proton.me>
2024-05-30 23:08:43 +1000

    Document the main module

--- spacecat/__main__.py
+++ spacecat/__main__.py
@@ -1,4 +1,10 @@
+"""
+This is the primary entry point, which just points to the cli module.
+
+It just runs the cli main function. Go there for more.
+"""
+
 from spacecat.cli import main
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
