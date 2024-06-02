976bb88b204db0caf68ac979dedb52ce28f6f295
Mizarc <mizarc@proton.me>
2024-06-02 15:44:34 +1000

    Clean up reaction buttons module for cleanup

--- spacecat/helpers/reaction_buttons.py
+++ spacecat/helpers/reaction_buttons.py
@@ -1,8 +1,32 @@
-def number_to_emoji(num: int):
-    emoji = f"{num}\u20e3"
-    return emoji
+"""
+This module provides utilities for reaction button type conversions.
+
+The current usage allows you to make use of emoji numbers are reaction
+buttons, and also to convert them back to their integer representations.
+"""
 
 
-def emoji_to_number(emoji: str):
-    number = int(emoji[0])
-    return number
+def number_to_emoji(num: int) -> str:
+    """
+    Converts a number to the emoji representation.
+
+    Args:
+        num (int): The number to convert.
+
+    Returns:
+        str: The emoji string representation of the number.
+    """
+    return f"{num}\u20e3"
+
+
+def emoji_to_number(emoji: str) -> int:
+    """
+    Converts a emoji number to the integer representation.
+
+    Args:
+        emoji (str): The emoji string to convert.
+
+    Returns:
+        int: The integer representation of the emoji.
+    """
+    return int(emoji[0])
