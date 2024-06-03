82598dcde1aa9c979fc8bfed4283aacd0944a047
Mizarc <mizarc@proton.me>
2024-06-04 01:11:59 +1000

    Move instance data out of constants

--- spacecat/helpers/constants.py
+++ spacecat/helpers/constants.py
@@ -17,69 +17,6 @@ CACHE_DIR = "cache/"
 DATA_DIR = "data/"
 
 
-class InstanceNameNotSetError(ValueError):
-    """
-    Raised when the `instance_name` is not set.
-
-    This is usually a sign that the instance name has not been
-    set prior to calling the `instance_location` function, which is
-    required to know where to store instance data.
-    """
-
-
-class InstanceData:
-    """
-    Class that holds instance specific data.
-
-    This class is used to store the instance name, which is used to
-    determine where instance data is stored.
-    """
-
-    def __init__(self: InstanceData) -> None:
-        """Initialize the InstanceData class."""
-        self._instance_name: str = ""
-
-    @property
-    def instance_name(self: Self) -> str:
-        """
-        Get the instance name.
-
-        Returns:
-            str: The name of the instance.
-        """
-        return self._instance_name
-
-    @instance_name.setter
-    def instance_name(self: Self, name: str) -> None:
-        """
-        Set the instance name.
-
-        Args:
-            name (str): The name of the instance.
-        """
-        InstanceData._instance_name = name
-
-    @property
-    def instance_location(self: Self) -> str:
-        """
-        Returns the location of the instance data directory.
-
-        This fetches the instance location by combining the data
-        directory with the instance name. Instance name must be set
-        prior to calling this.
-
-        Returns:
-            str: The location of the instance data directory.
-
-        Raises:
-            InstanceNameNotSetError: If the `instance_name` variable is
-                empty.
-        """
-        if InstanceData._instance_name == "":
-            raise InstanceNameNotSetError
-        return f"{DATA_DIR}/{InstanceData._instance_name}/"
-
-
 class EmbedStatus(enum.Enum):
     """
     An enum of statuses used for the colour of discord embeds.
