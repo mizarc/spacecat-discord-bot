884186547d83a34d120d94f9ff4ed5a2eb0ef47f
DESKTOP-939K91A\Mizarc <mizarc@proton.me>
2024-06-11 23:21:06 +1000

    Change command to app_comand in permission check
    The old way was a relic of the old command system, had to change it to
    the app_command system which required all ctx references to be changed
    to interaction.

--- spacecat/helpers/perms.py
+++ spacecat/helpers/perms.py
@@ -22,6 +22,26 @@ from discord.ext import commands
 from spacecat.helpers import constants
 
 
+def init_database() -> None:
+    """
+    Initializes the permissions databases.
+
+    This function should be run on bot start in order to ensure that
+    the tables that hold permissions are created if they do not exist.
+    """
+    db = sqlite3.connect(constants.DATA_DIR + "spacecat.db")
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS user_permission "
+        "(server_id INTEGER, user_id INTEGER, permission TEXT)"
+    )
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS group_permission "
+        "(server_id INTEGER, group_id INTEGER, permission TEXT)"
+    )
+    db.commit()
+    db.close()
+
+
 def new(guild: discord.Guild) -> None:
     """
     Initializes a new server configuration file for the given guild.
@@ -47,90 +67,85 @@ def check() -> Callable:
     """
     Check if user has permission to use the command.
 
-    Server administrators are granted all permissions. Otherwise, users
+    Server administrators are automatically granted permission. Otherwise, users
     are checked based on whether it is a globally granted permission, or
     whether it is overwritten on an individual server basis.
     """
 
-    async def predicate(ctx: commands.Context) -> bool:
+    def predicate(interaction: discord.Interaction) -> bool:
+        # You've put this decorator on the wrong function if this check triggers
+        if interaction.command is None:
+            return False
+
         # Check if command is being run outside of a guild or command is invalid
-        if not isinstance(ctx.author, discord.Member) or ctx.guild is None or ctx.command is None:
+        if not isinstance(interaction.user, discord.Member) or interaction.guild is None:
             return False
 
         # Allow if user is a server administrator
-        if ctx.author.guild_permissions.administrator:
+        if interaction.user.guild_permissions.administrator:
             return True
 
-        # Grab useful command variables
-        module = ctx.command.cog_name
-        command_values = [ctx.command.qualified_name]
-        if ctx.invoked_subcommand is not None:
-            command_values.extend(ctx.invoked_parents)
-            if ctx.invoked_subcommand.name is not None:
-                command_values.append(ctx.invoked_subcommand.name)
+        # Grab valid permissions from the command using its subcommand tree.
+        command_values = interaction.command.qualified_name.split(" ")
+        permissions = {"*", "." + ".".join(command_values)}
+        for i in range(len(command_values) - 1):
+            result = ".".join(command_values[: i + 1]) + ".*"
+            permissions.add(result)
 
-        # Add queries for the global, module, and command wildcards,
-        # plus the command on its own
-        perm = command_values[0]
-        checks = {"*", f"{module}.*", f"{module}.{perm}.*"}
         regex_query = re.compile("Preset.[a-zA-Z0-9]+")
-        for command_value in command_values[1:]:
-            perm = f"{perm}.{command_value}"
-            checks.add(f"{module}.{perm}.*")
-        checks.add(f"{module}.{perm}")
 
         # Query database to allow if user has the required permission
         db = sqlite3.connect(constants.DATA_DIR + "spacecat.db")
         db.row_factory = lambda _, row: row[0]
         cursor = db.cursor()
-        query = (ctx.guild.id, ctx.author.id)
+        query = (interaction.guild.id, interaction.user.id)
         cursor.execute(
-            "SELECT permission FROM user_permission " "WHERE server_id=? AND user_id=?", query
+            "SELECT permission FROM user_permission WHERE server_id=? AND user_id=?", query
         )
         user_results = cursor.fetchall()
         presets = list(filter(regex_query.match, user_results))
-        if set(user_results).intersection(checks):
+        if set(user_results).intersection(permissions):
             return True
 
         # Query database to allow if any group that the user is assigned
         # to has the required permission
-        for group in ctx.author.roles:
-            query = (ctx.guild.id, group.id)
+        for group in interaction.user.roles:
+            query = (interaction.guild.id, group.id)
             cursor.execute(
-                "SELECT permission FROM group_permission " "WHERE server_id=? AND group_id=?",
+                "SELECT permission FROM group_permission WHERE server_id=? AND group_id=?",
                 query,
             )
             group_results = cursor.fetchall()
             presets = presets + list(filter(regex_query.match, group_results))
-            if set(group_results).intersection(checks):
+            if set(group_results).intersection(permissions):
                 return True
 
             # Execute recurring parent check
-            parent_query = (ctx.guild.id, query[1])
-            parent_check, presets = _parent_perms(ctx, cursor, parent_query, checks, presets)
+            parent_query = (interaction.guild.id, query[1])
+            parent_check, presets = _parent_perms(
+                interaction, cursor, parent_query, permissions, presets
+            )
             if parent_check:
                 return True
 
         # Query permission presets from config that the user or group may have
         config = toml.load(constants.DATA_DIR + "config.toml")
         for preset in presets:
-            comparison = list(set(config["permissions"][preset[7:]]).intersection(checks))
+            comparison = list(set(config["permissions"][preset[7:]]).intersection(permissions))
             if comparison:
                 return True
 
         # Query config's default permission list
-        query = (ctx.guild.id,)
-        cursor.execute(
-            "SELECT advanced_permission FROM server_settings " "WHERE server_id=?", query
-        )
+        query = (interaction.guild.id,)
+        cursor.execute("SELECT advanced_permission FROM server_settings WHERE server_id=?", query)
         advanced = cursor.fetchone()
         if advanced == 0:
-            comparison = set(config["permissions"]["default"]).intersection(checks)
+            comparison = set(config["permissions"]["default"]).intersection(permissions)
             if comparison:
                 return True
         return False
 
-    return commands.check(predicate)
+    return discord.app_commands.check(predicate)
 
 
 def exclusive() -> Callable:
@@ -149,7 +164,11 @@ def exclusive() -> Callable:
 
 
 def _parent_perms(
-    ctx: commands.Context, cursor: sqlite3.Cursor, parent_query: tuple, checks: set, presets: list
+    interaction: discord.Interaction,
+    cursor: sqlite3.Cursor,
+    parent_query: tuple,
+    checks: set,
+    presets: list,
 ) -> tuple:
     """
     Recursively checks all parent permissions.
@@ -159,18 +178,18 @@ def _parent_perms(
     """
     # Check if group has parents
     cursor.execute(
-        "SELECT parent_id FROM group_parent " "WHERE server_id=? AND child_id=?", parent_query
+        "SELECT parent_id FROM group_parent WHERE server_id=? AND child_id=?", parent_query
     )
     parents = cursor.fetchall()
 
     # Check parent groups for permission
     for parent in parents:
-        if ctx.guild is None:
+        if interaction.guild is None:
             return False, presets
 
-        perm_query = (ctx.guild.id, parent)
+        perm_query = (interaction.guild.id, parent)
         cursor.execute(
-            "SELECT permission FROM group_permission " "WHERE server_id=? AND group_id=?",
+            "SELECT permission FROM group_permission WHERE server_id=? AND group_id=?",
             perm_query,
         )
         results = cursor.fetchall()
@@ -180,8 +199,10 @@ def _parent_perms(
             return True, presets
 
         # Check next parent level
-        new_parent_query = (ctx.guild.id, parent)
-        parent_check, presets = _parent_perms(ctx, cursor, new_parent_query, checks, presets)
+        new_parent_query = (interaction.guild.id, parent)
+        parent_check, presets = _parent_perms(
+            interaction, cursor, new_parent_query, checks, presets
+        )
         if parent_check:
             return True, presets
     return False, presets
