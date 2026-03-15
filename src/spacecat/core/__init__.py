1e13b8fc876f53ae27efe15301afefe51ecf8ffa
Mizarc <contact@mizarc.dev>
2026-03-16 01:34:32 +1000

    Fix social reference in init

--- src/spacecat/core/__init__.py
+++ src/spacecat/core/__init__.py
@@ -1,6 +1,6 @@
 """Core shared commands for all platforms."""
 
-from .features.fun import diceroll, coinflip
+from .features.social import diceroll, coinflip
 from .features.utility import echo, ping, uptime
 
 __all__ = ["diceroll", "coinflip", "echo", "ping", "uptime"]
