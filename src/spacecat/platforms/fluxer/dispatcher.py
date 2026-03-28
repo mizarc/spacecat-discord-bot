8bf661c22100797889f4dc02fd1bfc2b42eff073
Mizarc <contact@mizarc.dev>
2026-03-28 19:57:35 +1000

    Fix dispatch message for fluxer

--- src/spacecat/platforms/fluxer/dispatcher.py
+++ src/spacecat/platforms/fluxer/dispatcher.py
@@ -31,18 +31,42 @@ class FluxerDispatcher(BaseDispatcher):
         message = await self.bot.fetch_message(str(channel_id), str(message_id))
         await message.reply(content)
 
-    async def dispatch_message(self, channel_id: int, content: str):
-        channel = self.bot.get_channel(channel_id) or await self.bot.fetch_channel(channel_id)
+    async def dispatch_message(self, channel_id: int, content: str) -> None:
+        """
+        Dispatch a message to a specified channel.
+
+        Args:
+            channel_id: The ID of the channel to send the message to.
+            content: The content of the message.
+        """
+        channel = await self.bot.fetch_channel(str(channel_id))
         await channel.send(content)
 
     async def dispatch_embed(
         self, channel_id: int, title: str, description: str, color: int = 0x00FF00
-    ):
+    ) -> None:
+        """
+        Dispatch an embed message to a specified channel.
+
+        Args:
+            channel_id: The ID of the channel to send the embed to.
+            title: The title of the embed.
+            description: The description of the embed.
+            color: The color of the embed.
+        """
         embed = fluxer.Embed(title=title, description=description, color=color)
         channel = self.bot.get_channel(channel_id) or await self.bot.fetch_channel(channel_id)
         await channel.send(embed=embed)
 
-    async def dispatch_voice_move(self, guild_id: int, user_id: int, target_vc: int):
+    async def dispatch_voice_move(self, guild_id: int, user_id: int, target_vc: int) -> None:
+        """
+        Move a user to a different voice channel.
+
+        Args:
+            guild_id: The ID of the guild the user is in.
+            user_id: The ID of the user to move.
+            target_vc: The ID of the target voice channel.
+        """
         guild = self.bot.get_guild(guild_id)
         member = await guild.get_member(user_id)
         if member and member.voice:
